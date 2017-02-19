9754ec19595880beafe0c1ebd7ece45125e830d1
Anestis Mamplekos-Alexiou <anestis.mamplekos-alexiou.16@ucl.ac.uk>
2017-02-19 23:59:09 +0000

    Initialize Boids based on configuration file values

---
+++ Generate_Boids.py
@@ -0,0 +1,43 @@
+import random
+import yaml
+
+
+class Initialize(object):
+
+    def __init__(self):
+        self.configuration = yaml.load(open('config.yaml'))
+        self.birds_num = (self.configuration['birds_number'])
+
+        self.x_coord_low = (self.configuration['horizontal_position_bounds'][0])
+        self.x_coord_high = (self.configuration['horizontal_position_bounds'][1])
+
+        self.y_coord_low = (self.configuration['vertical_position_bounds'][0])
+        self.y_coord_high = (self.configuration['vertical_position_bounds'][1])
+
+        self.x_vel_low = (self.configuration['horizontal_velocity_bounds'][0])
+        self.x_vel_high = (self.configuration['horizontal_velocity_bounds'][1])
+
+        self.y_vel_low = (self.configuration['vertical_velocity_bounds'][0])
+        self.y_vel_high = (self.configuration['vertical_velocity_bounds'][1])
+
+    def set_initial_positions(self):
+        self.boids_x_scat = [random.uniform(self.x_coord_low, self.x_coord_high) for bird in range(self.birds_num)]
+        self.boids_y_scat = [random.uniform(self.y_coord_low, self.y_coord_high) for bird in range(self.birds_num)]
+        return [self.boids_x_scat, self.boids_y_scat]
+
+    def set_initial_velocities(self):
+        self.boids_x_vel_scat = [random.uniform(self.x_vel_low, self.x_vel_high) for bird in range(self.birds_num)]
+        self.boids_y_vel_scat = [random.uniform(self.y_vel_low, self.y_vel_high) for bird in range(self.birds_num)]
+        return [self.boids_x_vel_scat, self.boids_y_vel_scat]
+
+
+z = Initialize()
+a = z.set_initial_positions()
+b = z.set_initial_velocities()
+print(b[0])
+
+print(b[1])
+
+
+
+
