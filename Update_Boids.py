4f160ae16db209a08af64c9ca6d0e1e6bcf5c0fc
Anestis Mamplekos-Alexiou <anestis.mamplekos-alexiou.16@ucl.ac.uk>
2017-02-20 03:02:15 +0000

    Update Boids file: Replace hand-written code with library (Fly away from nearby birds function)

--- Update_Boids.py
+++ Update_Boids.py
@@ -1,4 +1,5 @@
 from Generate_Boids import Initialize
+import numpy as np
 import itertools
 
 
@@ -12,6 +13,8 @@ class Boids(object):
         self.birds_num = self.initial.birds_num
         self.alignment_const = (self.configuration['alignment_const'])
         self.separation_limit = (self.configuration['separation_limit'])
+        self.cohesion_limit = (self.configuration['cohesion_limit'])
+        self.cohesion_const = (self.configuration['cohesion_const'])
 
         # Get initial cartesian positions and velocities
         self.x = self.initial.initial_position()[0]
@@ -23,7 +26,7 @@ class Boids(object):
 
     def align(self):
 
-        # This function ascertains that the birds fly towards the middle line
+        # Fly towards the middle line
 
         for [bird_i, bird_j] in itertools.product(range(self.birds_num), repeat=2):
             self.x_vel[bird_i] = self.x_vel[bird_i] + (self.x[bird_j]-self.x[bird_i])*self.alignment_const/self.birds_num
@@ -31,14 +34,25 @@ class Boids(object):
 
         print(self.x_vel)
 
+    def separate(self):
 
+        # Fly away from nearby boids
 
+        print(self.x_vel)
 
+        for [bird_i, bird_j] in itertools.product(range(self.birds_num), repeat=2):
+            self.boid_a = np.array([self.x[bird_i], self.y[bird_i]])
+            self.boid_b = np.array([self.x[bird_j], self.y[bird_j]])
+            self.distance = (np.linalg.norm(self.boid_a - self.boid_b))**2
+            if self.distance < self.separation_limit:
+                self.x_vel[bird_i] = self.x_vel[bird_i] + (self.x[bird_i] - self.x[bird_j])
+                self.y_vel[bird_i] = self.y_vel[bird_i] + (self.y[bird_i] - self.y[bird_j])
 
+        print(self.x_vel)
 
 
 z = Boids()
-z.align()
+z.separate()
 
 
 
