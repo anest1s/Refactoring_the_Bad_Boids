b51dd8d56957a782eea8d5fa4d70fc3300a78b3a
Anestis Mamplekos-Alexiou <anestis.mamplekos-alexiou.16@ucl.ac.uk>
2017-02-21 19:09:01 +0000

    Replace repeated code with a funtion (squared difs). Smell: fragments of repeated code appear

--- Update_Boids.py
+++ Update_Boids.py
@@ -1,5 +1,4 @@
 from Generate_Boids import Initialize
-import numpy as np
 import itertools
 from matplotlib import pyplot as plt
 from matplotlib import animation
@@ -24,41 +23,46 @@ class Boids(object):
         self.y = self.initial.initial_position()[1]
         self.y_vel = self.initial.initial_velocity()[1]
 
+    def squared_dif(self, bird_i, bird_j):
+
+        # This function calculates the squared difference distance between birds
+        # Returns the squared dif distance
+
+        self.distance = (self.x[bird_i]-self.x[bird_j])**2 + (self.y[bird_i]-self.y[bird_j])**2
+        return self.distance
+
     def align(self):
 
         # Fly towards the middle line
 
         for [bird_i, bird_j] in itertools.product(range(self.birds_num), repeat=2):
-            self.x_vel[bird_i] = self.x_vel[bird_i] + (self.x[bird_j]-self.x[bird_i])*self.alignment_const/self.birds_num
-            self.y_vel[bird_i] = self.y_vel[bird_i] + (self.y[bird_j]-self.y[bird_i])*self.alignment_const/self.birds_num
+            self.x_vel[bird_i] += (self.x[bird_j]-self.x[bird_i])*self.alignment_const/self.birds_num
+            self.y_vel[bird_i] += (self.y[bird_j]-self.y[bird_i])*self.alignment_const/self.birds_num
 
     def separate(self):
 
         # Fly away from nearby boids
 
         for [bird_i, bird_j] in itertools.product(range(self.birds_num), repeat=2):
-            self.distance = (self.x[bird_i]-self.x[bird_j])**2 + (self.y[bird_i]-self.y[bird_j])**2
-            if self.distance < self.separation_limit:
-                self.x_vel[bird_i] = self.x_vel[bird_i] + (self.x[bird_i] - self.x[bird_j])
-                self.y_vel[bird_i] = self.y_vel[bird_i] + (self.y[bird_i] - self.y[bird_j])
+            if self.squared_dif(bird_i, bird_j) < self.separation_limit:
+                self.x_vel[bird_i] += (self.x[bird_i] - self.x[bird_j])
+                self.y_vel[bird_i] += (self.y[bird_i] - self.y[bird_j])
 
     def cohere(self):
 
         # Try to match speed with nearby birds
-
         for [bird_i, bird_j] in itertools.product(range(self.birds_num), repeat=2):
-            self.distance = (self.x[bird_i]-self.x[bird_j])**2 + (self.y[bird_i]-self.y[bird_j])**2
-            if self.distance < self.cohesion_limit:
-                self.x_vel[bird_i] = self.x_vel[bird_i] + (self.x_vel[bird_j]-self.x_vel[bird_i])*self.cohesion_const/self.birds_num
-                self.y_vel[bird_i] = self.y_vel[bird_i] + (self.y_vel[bird_j]-self.y_vel[bird_i])*self.cohesion_const/self.birds_num
+            if self.squared_dif(bird_i, bird_j) < self.cohesion_limit:
+                self.x_vel[bird_i] += (self.x_vel[bird_j]-self.x_vel[bird_i])*self.cohesion_const/self.birds_num
+                self.y_vel[bird_i] += (self.y_vel[bird_j]-self.y_vel[bird_i])*self.cohesion_const/self.birds_num
 
-    def new_positions(self):
+    def positions_velocities(self):
 
         # Move according to velocities
 
         for bird in range(self.birds_num):
-            self.x[bird] = self.x[bird] + self.x_vel[bird]
-            self.y[bird] = self.y[bird] + self.y_vel[bird]
+            self.x[bird] += self.x_vel[bird]
+            self.y[bird] += self.y_vel[bird]
 
     def update(self):
 
@@ -67,7 +71,7 @@ class Boids(object):
         self.align()
         self.separate()
         self.cohere()
-        self.new_positions()
+        self.positions_velocities()
 
 
 z = Boids()
