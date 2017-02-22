ee24edd3cfa751947f5cd22abf3b0f512f012630
Anestis Mamplekos-Alexiou <anestis.mamplekos-alexiou.16@ucl.ac.uk>
2017-02-22 19:25:50 +0000

    Improving Initialize class (Generate_Boids.py)

--- Generate_Boids.py
+++ Generate_Boids.py
@@ -7,24 +7,22 @@ class Initialize(object):
         self.configuration = yaml.load(open('config.yaml'))
 
         self.birds_num = (self.configuration['birds_number'])
-
-        self.position_lower_limits = np.array(self.configuration['position_lower_limits'])
-        self.position_upper_limits = np.array(self.configuration['position_upper_limits'])
-
-        self.velocity_lower_limits = np.array(self.configuration['velocity_lower_limits'])
-        self.velocity_upper_limits = np.array(self.configuration['velocity_upper_limits'])
-
+        
     def calc_initial_conditions(self, lower_limits, upper_limits):
         width = upper_limits - lower_limits
         condition = lower_limits[:, np.newaxis] + np.random.rand(2, self.birds_num)*width[:, np.newaxis]
         return condition
 
     def initial_position(self):
-        position = self.calc_initial_conditions(self.position_lower_limits, self.position_upper_limits)
+        position_lower_limits = np.array(self.configuration['position_lower_limits'])
+        position_upper_limits = np.array(self.configuration['position_upper_limits'])
+        position = self.calc_initial_conditions(position_lower_limits, position_upper_limits)
         return position
 
     def initial_velocity(self):
-        velocity = self.calc_initial_conditions(self.velocity_lower_limits, self.velocity_upper_limits)
+        velocity_lower_limits = np.array(self.configuration['velocity_lower_limits'])
+        velocity_upper_limits = np.array(self.configuration['velocity_upper_limits'])
+        velocity = self.calc_initial_conditions(velocity_lower_limits, velocity_upper_limits)
         return velocity
 
 '''
