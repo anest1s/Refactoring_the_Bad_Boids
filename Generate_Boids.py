9d72f36b2a9345d112e8f013d11ae5d14d372634
Anestis Mamplekos-Alexiou <anestis.mamplekos-alexiou.16@ucl.ac.uk>
2017-02-22 00:39:56 +0000

    Refactoring: Replace hand written code with library (Generate Boids: Initialize class)

--- Generate_Boids.py
+++ Generate_Boids.py
@@ -1,5 +1,5 @@
-import random
 import yaml
+import numpy as np
 
 
 class Initialize(object):
@@ -8,34 +8,23 @@ class Initialize(object):
         self.configuration = yaml.load(open('config.yaml'))
         self.birds_num = (self.configuration['birds_number'])
 
-        self.x_coord_low = (self.configuration['horizontal_position_bounds'][0])
-        self.x_coord_high = (self.configuration['horizontal_position_bounds'][1])
+        self.position_lower_limits = np.array(self.configuration['position_lower_limits'])
+        self.position_upper_limits = np.array(self.configuration['position_upper_limits'])
 
-        self.y_coord_low = (self.configuration['vertical_position_bounds'][0])
-        self.y_coord_high = (self.configuration['vertical_position_bounds'][1])
-
-        self.x_vel_low = (self.configuration['horizontal_velocity_bounds'][0])
-        self.x_vel_high = (self.configuration['horizontal_velocity_bounds'][1])
-
-        self.y_vel_low = (self.configuration['vertical_velocity_bounds'][0])
-        self.y_vel_high = (self.configuration['vertical_velocity_bounds'][1])
+        self.velocity_lower_limits = np.array(self.configuration['velocity_lower_limits'])
+        self.velocity_upper_limits = np.array(self.configuration['velocity_upper_limits'])
 
     def initial_position(self):
-        self.boids_x_scat = [random.uniform(self.x_coord_low, self.x_coord_high) for bird in range(self.birds_num)]
-        self.boids_y_scat = [random.uniform(self.y_coord_low, self.y_coord_high) for bird in range(self.birds_num)]
-        return [self.boids_x_scat, self.boids_y_scat]
+        self.width = self.position_upper_limits - self.position_lower_limits
+        self.position = self.position_lower_limits[:, np.newaxis] + np.random.rand(2, self.birds_num)*self.width[:, np.newaxis]
+        return self.position
 
     def initial_velocity(self):
-        self.boids_x_vel_scat = [random.uniform(self.x_vel_low, self.x_vel_high) for bird in range(self.birds_num)]
-        self.boids_y_vel_scat = [random.uniform(self.y_vel_low, self.y_vel_high) for bird in range(self.birds_num)]
-        return [self.boids_x_vel_scat, self.boids_y_vel_scat]
+        self.width = self.velocity_upper_limits - self.velocity_lower_limits
+        self.velocity = self.velocity_lower_limits[:, np.newaxis] + np.random.rand(2, self.birds_num)*self.width[:, np.newaxis]
+        return self.velocity
 
 
-'''
 z = Initialize()
 a = z.initial_position()
-b = z.initial_velocity()
-print(b[0])
-
-print(b[1])
-'''
+print(a)
