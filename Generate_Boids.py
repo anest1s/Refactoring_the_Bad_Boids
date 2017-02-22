32fa029b412164c61e8ac2d23c66ee808c867c47
Anestis Mamplekos-Alexiou <anestis.mamplekos-alexiou.16@ucl.ac.uk>
2017-02-22 01:42:19 +0000

    Refactoring: Replace repeated code with a function (Generate_Boids.py: calc_initial_conditions function)

--- Generate_Boids.py
+++ Generate_Boids.py
@@ -3,9 +3,9 @@ import numpy as np
 
 
 class Initialize(object):
-
     def __init__(self):
         self.configuration = yaml.load(open('config.yaml'))
+
         self.birds_num = (self.configuration['birds_number'])
 
         self.position_lower_limits = np.array(self.configuration['position_lower_limits'])
@@ -14,14 +14,17 @@ class Initialize(object):
         self.velocity_lower_limits = np.array(self.configuration['velocity_lower_limits'])
         self.velocity_upper_limits = np.array(self.configuration['velocity_upper_limits'])
 
+    def calc_initial_conditions(self, lower_limits, upper_limits):
+        self.width = upper_limits - lower_limits
+        self.condition = lower_limits[:, np.newaxis] + np.random.rand(2, self.birds_num)*self.width[:, np.newaxis]
+        return self.condition
+
     def initial_position(self):
-        self.width = self.position_upper_limits - self.position_lower_limits
-        self.position = self.position_lower_limits[:, np.newaxis] + np.random.rand(2, self.birds_num)*self.width[:, np.newaxis]
+        self.position = self.calc_initial_conditions(self.position_lower_limits, self.position_upper_limits)
         return self.position
 
     def initial_velocity(self):
-        self.width = self.velocity_upper_limits - self.velocity_lower_limits
-        self.velocity = self.velocity_lower_limits[:, np.newaxis] + np.random.rand(2, self.birds_num)*self.width[:, np.newaxis]
+        self.velocity = self.calc_initial_conditions(self.velocity_lower_limits, self.velocity_upper_limits)
         return self.velocity
 
 
