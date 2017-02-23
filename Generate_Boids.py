c0a821b597fb39ed53b733b1eb4bdc3b3efb0924
Anestis Mamplekos-Alexiou <anestis.mamplekos-alexiou.16@ucl.ac.uk>
2017-02-23 01:07:42 +0000

    Refactoring to inheritance: repeated code between Initialize and Boids Classes

--- Generate_Boids.py
+++ Generate_Boids.py
@@ -6,24 +6,30 @@ class Initialize(object):
     def __init__(self):
         self.config = yaml.load(open('config.yaml'))
 
+        # simulation constants
         self.birds_num = (self.config['birds_number'])
+        self.alignment_const = (self.config['alignment_const'])
+        self.separation_limit = (self.config['separation_limit'])
+        self.cohesion_limit = (self.config['cohesion_limit'])
+        self.cohesion_const = (self.config['cohesion_const'])
+
+        # random initial positions and velocities
+        self.positions = self.initial_conditions(self.config['position_lower_limits'],
+                                                 self.config['position_upper_limits'])
+
+        self.velocities = self.initial_conditions(self.config['velocity_lower_limits'],
+                                                  self.config['velocity_upper_limits'])
 
     def initial_conditions(self, lower_limits, upper_limits):
+
+        '''
+        :param lower_limits: position or velocity lower limits
+        :param upper_limits: position or velocity upper limits
+        This function calculates the condition (position or velocity) of the birds based on the input data (config_file)
+        :return:
+        '''
+
         lower_limits, upper_limits = [np.array(lower_limits), np.array(upper_limits)]
         width = upper_limits - lower_limits
         condition = lower_limits[:, np.newaxis] + np.random.rand(2, self.birds_num)*width[:, np.newaxis]
         return condition
-
-    def initial_position(self):
-        position = self.initial_conditions(self.config['position_lower_limits'], self.config['position_upper_limits'])
-        return position
-
-    def initial_velocity(self):
-        velocity = self.initial_conditions(self.config['velocity_lower_limits'], self.config['velocity_upper_limits'])
-        return velocity
-
-'''
-z = Initialize()
-a = z.initial_position()
-print(a)
-'''
--- Update_Boids.py
+++ Update_Boids.py
@@ -4,46 +4,35 @@ from matplotlib import animation
 import numpy as np
 
 
-class Boids(object):
+class Boids(Initialize):
     def __init__(self):
-
-        self.initial = Initialize()  # Create object from Initialize class
-        self.configuration = self.initial.config  # Get configuration data
-
-        # Get configuration constants
-        self.birds_num = self.initial.birds_num
-        self.alignment_const = (self.configuration['alignment_const'])
-        self.separation_limit = (self.configuration['separation_limit'])
-        self.cohesion_limit = (self.configuration['cohesion_limit'])
-        self.cohesion_const = (self.configuration['cohesion_const'])
-
-        # Get initial cartesian positions and velocities
-
-        self.positions = self.initial.initial_position()
-        self.velocities = self.initial.initial_velocity()
-
-    def sum_squared_dif(self):
-
-        # This function calculates the squared difference distance between birds
-        # Returns the squared dif distance
-        separations = self.positions[:, np.newaxis, :] - self.positions[:, :, np.newaxis]
-        squared_displacements = separations * separations
-        self.separations_if_close = np.copy(separations)
-        ssd = np.sum(squared_displacements, 0)
-        return ssd
+        Initialize.__init__(self)
+        super(Boids, self).__init__()
 
     def align(self):
 
         # Fly towards the middle line
+
         middle = np.mean(self.positions, 1)
         direction_to_middle = self.positions - middle[:, np.newaxis]
         self.velocities -= direction_to_middle*self.alignment_const
+
+    def square_distances(self):
+
+        # This function calculates the squared difference distance between birds
+        # Returns the squared dif distance
+
+        separations = self.positions[:, np.newaxis, :] - self.positions[:, :, np.newaxis]
+        squared_displacements = separations * separations
+        self.separations_if_close = np.copy(separations)
+        square_dist = np.sum(squared_displacements, 0)
+        return square_dist
 
     def separate(self):
 
         # Fly away from nearby boids
 
-        far_away = self.sum_squared_dif() > self.separation_limit
+        far_away = self.square_distances() > self.separation_limit
         self.separations_if_close[0, :, :][far_away] = 0
         self.separations_if_close[1, :, :][far_away] = 0
         self.velocities += np.sum(self.separations_if_close, 1)
@@ -54,8 +43,9 @@ class Boids(object):
 
         velocities_differences = self.velocities[:, np.newaxis, :] - self.velocities[:, :, np.newaxis]
         velocities_differences_if_close = np.copy(velocities_differences)
-        velocities_differences_if_close[0, :, :][self.sum_squared_dif() > self.cohesion_limit] = 0
-        velocities_differences_if_close[1, :, :][self.sum_squared_dif() > self.cohesion_limit] = 0
+        far_away = self.square_distances() > self.cohesion_limit
+        velocities_differences_if_close[0, :, :][far_away] = 0
+        velocities_differences_if_close[1, :, :][far_away] = 0
         self.velocities -= np.mean(velocities_differences_if_close, 1)*self.cohesion_const
 
     def positions_velocities(self):
@@ -66,7 +56,7 @@ class Boids(object):
 
     def update(self):
 
-        # Update Boids method
+        # Update Boids combining all methods
 
         self.align()
         self.separate()
@@ -75,6 +65,7 @@ class Boids(object):
 
 
 z = Boids()
+print(z.birds_num)
 
 figure = plt.figure()
 axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
