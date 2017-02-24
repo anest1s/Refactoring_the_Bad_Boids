8f6c6f4dd56dd78c8c761cff2f57700e51c8fb94
Anestis Mamplekos-Alexiou <anestis.mamplekos-alexiou.16@ucl.ac.uk>
2017-02-24 14:15:43 +0000

    Initialization test added

--- Good_Boids_module/tests/test_the_Good_Boids.py
+++ Good_Boids_module/tests/test_the_Good_Boids.py
@@ -8,22 +8,43 @@ import yaml
 from Good_Boids_module.tests.record_fixtures import configuration_file
 
 
-def test_Good_boids_for_regression():
-    regression_data = yaml.load(open('fixture.yaml'))
+fixtures = yaml.load(open('fixture.yaml'))
+configuration_file_data = yaml.load(open(configuration_file))
 
-    before_positions = list(regression_data["before_positions"])
-    before_velocities = list(regression_data["before_velocities"])
+
+def test_good_boids_for_regression():
+
+    before_positions = list(fixtures["before_positions"])
+    before_velocities = list(fixtures["before_velocities"])
 
     new_positions = list(Boids(configuration_file).get_raw_positions(before_positions, before_velocities))
-    after_positions = list(regression_data["after_positions"])
+    after_positions = list(fixtures["after_positions"])
 
     new_velocities = list(Boids(configuration_file).get_raw_velocities(before_positions, before_velocities))
-    after_velocities = list(regression_data["after_velocities"])
+    after_velocities = list(fixtures["after_velocities"])
 
     for i in range(len(new_positions)):
         assert_almost_equal(new_positions[0][i], after_positions[0][i], delta=0.1)
         assert_almost_equal(new_positions[1][i], after_positions[1][i], delta=0.1)
-        assert_almost_equal(new_velocities[0][i], after_velocities[0][i], delta=3)
-        assert_almost_equal(new_velocities[1][i], after_velocities[1][i], delta=3)
+        assert_almost_equal(new_velocities[0][i], after_velocities[0][i], delta=5)
+        assert_almost_equal(new_velocities[1][i], after_velocities[1][i], delta=5)
 
-test_Good_boids_for_regression()
+test_good_boids_for_regression()
+
+
+def test_good_boids_initialization():
+    boids_positions = Boids(configuration_file).positions
+    boids_velocities = Boids(configuration_file).velocities
+    assert_equal(configuration_file_data['birds_number'], len(boids_positions[0]))
+    assert_equal(configuration_file_data['birds_number'], Boids(configuration_file).birds_num)
+    for boid in range(Boids(configuration_file).birds_num):
+        assert_less(boids_positions[0][boid], configuration_file_data['position_upper_limits'][0])
+        assert_greater(boids_positions[0][boid], configuration_file_data['position_lower_limits'][0])
+        assert_less(boids_positions[1][boid], configuration_file_data['position_upper_limits'][1])
+        assert_greater(boids_positions[1][boid], configuration_file_data['position_lower_limits'][1])
+        assert_less(boids_velocities[0][boid], configuration_file_data['velocity_upper_limits'][0])
+        assert_greater(boids_velocities[0][boid], configuration_file_data['velocity_lower_limits'][0])
+        assert_less(boids_velocities[1][boid], configuration_file_data['velocity_upper_limits'][1])
+        assert_greater(boids_velocities[1][boid], configuration_file_data['velocity_lower_limits'][1])
+
+test_good_boids_initialization()
