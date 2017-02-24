cda6bfa68dbde02a631bda9ff3683ad632231030
Anestis Mamplekos-Alexiou <anestis.mamplekos-alexiou.16@ucl.ac.uk>
2017-02-24 05:34:12 +0000

    Refactoring: Inheritance Initializer-->Boids-->Animator

--- Good_Boids_module/Animate_Boids.py
+++ Good_Boids_module/Animate_Boids.py
@@ -1,10 +1,9 @@
 from matplotlib import animation
 from matplotlib import pyplot as plt
-from Good_Boids_module.Generate_Boids import Initializer
 from Good_Boids_module.Update_Boids import Boids
 
 
-class Animator(Initializer, Boids):
+class Animator(Boids):
     def __init__(self, configuration_file):
         super(Animator, self).__init__(configuration_file)
 
@@ -16,7 +15,7 @@ class Animator(Initializer, Boids):
 
         figure = plt.figure()
         axes = plt.axes(xlim=self.config['x_coordinates'], ylim=self.config['y_coordinates'])
-        positions = self.boids.positions
+        positions = self.positions
         scatter = axes.scatter(positions[0, :], positions[1, :])
 
         # Function handle for animate
